1dc2b0b553e62ef1663ed6b790be8d6cd175c69c
Artem Kurilik <artem.kurilik@gmail.com>
2023-09-22 12:56:34 +0300

    Fix imports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { useClassName } from './useClassName'
 export { useWindowHeight } from './useWindowHeight'
 export { useWindowSize } from './useWindowSize'
 export { useWindowWidth } from './useWindowWidth'
-
+export { useWindowScrollY } from './useWindowScrollY'
